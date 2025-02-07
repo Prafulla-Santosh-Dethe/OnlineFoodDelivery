a59e9316e6d933e0437ef2c165f76a58f80ce063
Prafulla_Dethe <dethe27999@gmail.com>
2025-02-07 12:30:53 +0530

    Updates

---
+++ Grocery.529bf84b.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire94c2;(0,e.register)("8WJSu",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var i=e("l6Z0V"),l=()=>(console.log("inside Grocery"),(0,i.jsx)("div",{children:(0,i.jsx)("h1",{children:"Inside Grocery Component"})}))});
+//# sourceMappingURL=Grocery.529bf84b.js.map
---
+++ Grocery.5f6ebf86.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire0da5;(0,e.register)("8WJSu",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var i=e("l6Z0V"),l=()=>(console.log("inside Grocery"),/*#__PURE__*/(0,i.jsx)("div",{children:/*#__PURE__*/(0,i.jsx)("h1",{children:"Inside Grocery Component"})}))});
+//# sourceMappingURL=Grocery.5f6ebf86.js.map
